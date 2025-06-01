98099fe1e12e5bf3c5bb6ae165ace5bcb9782c3d
BillHebertPDC <bill.choi@sptech.school.com>
2025-06-01 00:35:18 -0300

    feat(middleware): adiciona middleware de autenticação JWT
    Implementa middleware  que verifica o token JWT nas rotas protegidas, permitindo acesso condicional a rotas seguras, com tratamento de erros e redirecionamento para login em caso de falha.

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+let safe_routes = ["/postagem/codigo"]
+let safe_routes_get = ["/postagem/codigo"]
+export async function Auth(req, res, next) {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (process.env.DEV == "true")
+        console.log(req.path, req.method)
+    if (!token) {
+        if (safe_routes.includes(req.path))
+            return next()
+        if (req.method == "GET")
+            return next()
+        return res.redirect("/")
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+        return;
+    } catch (err) {
+        if (safe_routes.includes(req.path))
+            return next()
+        return res.redirect("/")
+    }
+}
