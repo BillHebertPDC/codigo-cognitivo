757480b46ae348cef6b6b29e89eedb6ed68460ec
BillHebertPDC <bill.choi@sptech.school.com>
2025-06-01 00:55:06 -0300

    feat(controller/conteudo): importação do service de conteudo, distribuição dos parametros para o service e resposta OK padrão

---
+++ src/controllers/postagem/conteudo/create.js
@@ -0,0 +1,13 @@
+import { log } from "../../../utils/log.js";
+import { servicesPostagemConteudoCreate } from "../../../services/postagem/Conteudo/create.js";
+export const controllersPostagemConteudoCreate = async (req, res, next) => {
+    try {
+        const PARAMETROS = req.params;
+        const { titulo, explicacao, Conteudo, nivel } = req.body
+        await servicesPostagemConteudoCreate({ titulo: titulo, explicacao: explicacao, Conteudo: Conteudo, nivel: nivel })
+        return res.status(200).json({ OK: true });
+    } catch (e) {
+        log(import.meta.url, e)
+        next(e, req, res, next)
+    }
+}
