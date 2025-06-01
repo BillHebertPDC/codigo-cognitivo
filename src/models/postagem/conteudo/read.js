4bb20c05754c6c4a79f5b949871749597c52576a
BillHebertPDC <bill.choi@sptech.school.com>
2025-06-01 01:46:08 -0300

    feat(models/postagem/conteudo): parte responsavel pelos selects de conteudo

---
+++ src/models/postagem/conteudo/read.js
@@ -0,0 +1,21 @@
+import { executar } from "../../../utils/mysql.js";
+import { log } from "../../../utils/log.js";
+
+export async function modelsPostagemConteudoRead({ id, dificuldade }) {
+    try {
+        const conditions = [];
+        if (dificuldade !== undefined && dificuldade !== null && dificuldade != "X") {
+            conditions.push(`dificuldade in (${dificuldade})`);
+        }
+        if (id !== undefined && id !== null) {
+            conditions.push(`id in (${id})`);
+        }
+        const where = conditions.length ? `WHERE ${conditions.join(" AND ")}` : "";
+        const sql = `SELECT * FROM conteudos ${where}`;
+        return await executar(sql);
+
+    } catch (e) {
+        log(import.meta.url, e);
+        throw "ERRO NO BANCO";
+    }
+}
