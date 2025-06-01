4fab869fd6001f3c3cc07348f3cc7c5fc7405439
BillHebertPDC <bill.choi@sptech.school.com>
2025-06-01 01:38:29 -0300

    fix(controller/postagem/conteudo) correção para o padrão antes conteudo para descrição

--- src/controllers/postagem/conteudo/create.js
+++ src/controllers/postagem/conteudo/create.js
@@ -3,8 +3,8 @@ import { servicesPostagemConteudoCreate } from "../../../services/postagem/conte
 export const controllersPostagemConteudoCreate = async (req, res, next) => {
     try {
         const PARAMETROS = req.params;
-        const { titulo, explicacao, Conteudo, nivel } = req.body
-        await servicesPostagemConteudoCreate({ titulo: titulo, explicacao: explicacao, Conteudo: Conteudo, nivel: nivel })
+        const { titulo, descricao, nivel } = req.body
+        await servicesPostagemConteudoCreate({ titulo: titulo, descricao: descricao,  nivel: nivel })
         return res.status(200).json({ OK: true });
     } catch (e) {
         log(import.meta.url, e)
